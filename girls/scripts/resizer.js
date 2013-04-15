4e249fa9f603fdcaf1914175b46f9514e4f1f66d
Tom Liversidge <tom@liversidge.me>
2013-04-15 12:01:11 +0100

    adding resizer.js

---
+++ girls/scripts/resizer.js
@@ -0,0 +1,12 @@
+define({
+    resizeText: function(el)
+    {
+        el.setAttribute("style", "font-size:" + this.getWidth() + "px; top:" + this.getHeight() + "px;");
+    },
+    getWidth: function () {
+        return (window.innerWidth || document.documentElement.clientWidth) / 3.5;
+        },
+    getHeight: function () {
+        return (window.innerHeight || document.documentElement.clientHeight) / 6.5;
+        }
+});
